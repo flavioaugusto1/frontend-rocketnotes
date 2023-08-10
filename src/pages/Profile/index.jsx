38a5567964c68455ff34ef712265bcedb5d2b057
Flávio <f.augustosdn.dev@gmail.com>
2023-08-09 23:26:28 -0300

    Created profile screen

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,39 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+
+import { Container, Form, Avatar } from "./styles";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="#">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Avatar>
+        <img
+          src="https://github.com/flavioaugusto1.png"
+          alt="imagem do usuário"
+        />
+
+        <label htmlFor="avatar">
+          <FiCamera />
+          <input id="avatar" type="file" />
+        </label>
+      </Avatar>
+
+      <Form>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
