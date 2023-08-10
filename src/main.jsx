2977a879636e98db12cf0eb09393ccacbe00bcb7
Flávio <f.augustosdn.dev@gmail.com>
2023-08-09 22:46:55 -0300

    Change signup to signin

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "./styles/global";
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
